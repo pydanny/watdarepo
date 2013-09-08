e942ceb2ba9e8d2202bb83057b6a452391b7bf7e
Daniel Greenfeld <pydanny@gmail.com>
2013-09-08 13:05:45 +0200

    Path corrections

--- watdarepo/__init__.py
+++ watdarepo/__init__.py
@@ -5,6 +5,6 @@ __author__ = 'Daniel Greenfeld'
 __email__ = 'pydanny@gmail.com'
 __version__ = '0.1.0'
 
-from main import watdarepo
-from main import identify_vcs
-from main import identify_hosting_service
+from .main import watdarepo
+from .main import identify_vcs
+from .main import identify_hosting_service
